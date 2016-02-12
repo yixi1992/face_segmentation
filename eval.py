cff24f547d00ff9aec5d13fd222549bdc9e3ac83
yixi1992 <yixi@login-1.deepthought2.umd.edu>
2016-02-12 09:37:32 -0500

    add model version

--- eval.py
+++ eval.py
@@ -12,8 +12,8 @@ Rwidth=100
 LabelHeight=100
 LabelWidth=100
 
-#dataset='Train'
-dataset='Test'
+dataset='Train'
+#dataset='Test'
 image_dir='/lustre/yixi/data/massimomauro-FASSEG-dataset-f93e332/V2/'+dataset+'_RGB/*.bmp'
 image_files = sorted(glob.glob(image_dir))
 label_dir='/lustre/yixi/data/massimomauro-FASSEG-dataset-f93e332/V2/'+dataset+'_Labels/labels/'
@@ -60,18 +60,20 @@ def test_accuracy(model_file):
 
 
 
-iter = range(10, 62975, 1000)
+iter = range(100, 71350, 1000)
 model_acc = np.zeros(len(iter))
+version = 'fixlr1e-8_62970+'
 for idx,i in enumerate(iter):
-	model_file = 'snapshots/snapshot_face_segmentation_finetune_fixlr1e-8__iter_'+str(i)+'.caffemodel.h5'
-	if not os.path.exists('pred_visual/'+model_file[len('snapshots/snapshot_face_segmentation_finetune_'):len('snapshots/snapshot_face_segmentation_finetune_fixlr1e-8__iter_'+str(i))] + '/'):
-		os.makedirs('pred_visual/'+model_file[len('snapshots/snapshot_face_segmentation_finetune_'):len('snapshots/snapshot_face_segmentation_finetune_fixlr1e-8__iter_'+str(i))] + '/')
+	model_file = 'snapshots/snapshot_face_segmentation_finetune_'+version+'_iter_'+str(i)+'.caffemodel.h5'
+	pred_visual_dir = 'pred_visual/'+version+'_iter_'+str(i)+'/'
+	if not os.path.exists(pred_visual_dir):
+		os.makedirs(pred_visual_dir)
 	model_acc[idx] = test_accuracy(model_file)
 	#
 	plt.clf()
 	plt.plot(model_acc[:(idx+1)])
 	plt.ylabel('accuracy')
 	plt.title('accuracy on '+dataset)
-	plt.savefig(dataset+'_accuracy_fixlr1e-8.png')
+	plt.savefig(dataset+'_accuracy_'+version+'.png')
 	
 
