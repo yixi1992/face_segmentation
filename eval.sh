7ca53f5831462bf36acc581aa6a617624a347058
yixi1992 <yixi@login-1.deepthought2.umd.edu>
2016-02-26 19:03:14 -0500

    update

--- eval.sh
+++ eval.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#SBATCH -t 0:30:00
-#SBATCH -N 1
+#SBATCH -t 10:00:00
 
 source /lustre/yixi/decouplednet/DecoupledNet/inference/load_deeplab_dependencies.sh
 
-python eval.py
+python eval.py > eval.out
 #./caffe/build/tools/caffe train -solver face_segmentation_finetune_solver.prototxt -weights ../image+jit2d+illum+rot3d/snapshots/snapshot_janus_baseline_iter_401000.caffemodel.h5 -gpu 0
