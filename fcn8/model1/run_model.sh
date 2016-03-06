141c0122219bb03cae4e0fa00a9afdb3fd089a97
yixi1992 <yixi1992@gmail.com>
2016-03-05 19:56:04 -0500

    fcn8 continue training

--- fcn8/model1/run_model.sh
+++ fcn8/model1/run_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -t 2:00:00
+#SBATCH -t 4:00:00
 #SBATCH -N 1
 #SBATCH --gres=gpu
 
--- fcn8/model1/solve.py
+++ fcn8/model1/solve.py
@@ -34,7 +34,8 @@ def interp_surgery(net, layers):
 # base_weights = 'vgg16fc.caffemodel'
 # base_weights = '../VGG16fc.caffemodel'
 # base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/model2/snapshots_camvid300/train_lr1e-12/_iter_12300.caffemodel'
-base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/model2/snapshots_camvid300/train_lr1e-12_12300_lr1e-10_12500/_iter_13000.caffemodel'
+# base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/model2/snapshots_camvid300/train_lr1e-12_12300_lr1e-10_12500/_iter_13000.caffemodel'
+base_weights = '/lustre/yixi/face_segmentation_finetune/fcn8/model1/snapshots_camvid300/train_lr1e-10w0.0005/_iter_12000.caffemodel'
 
 # init
 caffe.set_mode_gpu()
