f480571874bcd98cf00407b744bbd4880e66b80a
yixi1992 <yixi1992@gmail.com>
2016-04-08 21:39:42 -0400

    generate flow

--- gcfv_flow/genflow/genflow.m
+++ gcfv_flow/genflow/genflow.m
@@ -1,6 +1,7 @@
-
+function genflow(proc_rank, proc_size),
 
 addpath(genpath('epicflow'))
+addpath(genpath('utils'));
 
 FRAME_DIR = '/lustre/yixi/data/gcfv_dataset/cross_validation/videos/frames/%s.jpg';
 FLOW_SAVE_DIR = '/lustre/yixi/data/gcfv_dataset/cross_validation/videos/flo/%s.flo';
@@ -12,33 +13,39 @@ files = files(~[files.isdir]);
 
 
 for i=1:length(files),
-	[pathstr, name1, ext1] = fileparts(files(i).name);
-	parts = strread(name1,'%s','delimiter','_');
-	vid = str2num(parts{1});
-	fid = str2num(parts{2});
-	name2 = [num2str(vid), '_', num2str(fid+1)];
-	
-	if exist(sprintf(FRAME_DIR, name2), 'file')~=2,
-		continue;
+	if mod(i, proc_size) == proc_rank,
+		[pathstr, name1, ext1] = fileparts(files(i).name);
+		parts = strread(name1,'%s','delimiter','_');
+		vid = str2num(parts{1});
+		fid = str2num(parts{2});
+		name2 = [num2str(vid), '_', num2str(fid+1)];
+		
+		backward = 0;
+		if exist(sprintf(FRAME_DIR, name2), 'file')~=2,
+			if exist(sprintf(FRAME_DIR, [num2str(vid), '_', num2str(fid-1)]))==2,
+				name2 = sprintf(FRAME_DIR, [num2str(vid), '_', num2str(fid-1)]);
+				backward = 1;
+			else
+				continue;
+			end
+		end
+
+		IMAGE1_PATH = sprintf(FRAME_DIR, name1)
+		IMAGE2_PATH = sprintf(FRAME_DIR, name2)
+		FLOW_SAVE_PATH = sprintf(FLOW_SAVE_DIR, name1)
+
+		f = get_epicflow(IMAGE1_PATH, IMAGE2_PATH, FLOW_SAVE_PATH);
+		
+		if backward==1,
+			flow_img = readFlowFile(f);
+			writeFlowFile(flow_img*(-1), f);
+
+			%imwrite(flow_img(:,:,1), sprintf(FLOW_X_SAVE_DIR, name1));
+			%imwrite(flow_img(:,:,2), sprintf(FLOW_Y_SAVE_DIR, name1));
+		end
+
 	end
-
-	IMAGE1_PATH = sprintf(FRAME_DIR, name1)
-	IMAGE2_PATH = sprintf(FRAME_DIR, name2)
-	FLOW_SAVE_PATH = sprintf(FLOW_SAVE_DIR, name1)
-
-	f = get_epicflow(IMAGE1_PATH, IMAGE2_PATH, FLOW_SAVE_PATH);
-	addpath(genpath('utils'));
-	flow_img = readFlowFile(f);
-	% width*height*2 real numbers double optical flow
-	imwrite(flow_img(:,:,1), sprintf(FLOW_X_SAVE_DIR, name1));
-	imwrite(flow_img(:,:,2), sprintf(FLOW_Y_SAVE_DIR, name1));
 end
 
-
-
-%{
-FOLDER_PATH='/lustre/yixi/data/gcfv_dataset/cross_validation/videos/frames/'
-FLOW_SAVE_FOLDER_PATH='/lustre/yixi/data/gcfv_dataset/cross_validation/videos/flow/'
-dir_get_epicflow(FOLDER_PATH, FLOW_SAVE_FOLDER_PATH)
-%}
+end
 
