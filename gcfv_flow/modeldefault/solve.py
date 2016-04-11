3abdab56536f2e40ea3898bb5083f2058c1ab1a2
yixi1992 <yixi1992@gmail.com>
2016-04-11 02:08:20 -0400

    best from vgg is 28k vgg—â_lr1e-10

--- gcfv_flow/modeldefault/solve.py
+++ gcfv_flow/modeldefault/solve.py
@@ -31,7 +31,17 @@ def interp_surgery(net, layers):
 # base net -- follow the editing model parameters example to make
 # a fully convolutional VGG16 net.
 # http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/net_surgery.ipynb
+# from VGG16
+#base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/VGG16fc.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfv200200/vgg_lr1e-10/_iter_5000.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfvshuffle200200/vgg_lr1e-10/_iter_45000.caffemodel'
+
+# from Camvid finetuned
 base_weights = 'camvid_modeldefault_surg.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfv200200/train_lr1e-10/_iter_7000.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfv200200/train_lr1e-10_7000/_iter_15000.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfv200200/train_lr1e-10_7000_15000/_iter_30000.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfvshuffle200200/train_lr1e-10/_iter_30000.caffemodel'
 
 # init
 caffe.set_mode_gpu()
@@ -40,11 +50,25 @@ caffe.set_device(0)
 solver = caffe.SGDSolver('solver.prototxt')
 
 # do net surgery to set the deconvolution weights for bilinear interpolation
-#interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
-#interp_surgery(solver.net, interp_layers)
+interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+interp_surgery(solver.net, interp_layers)
+print interp_layers
+print '----- yixi initialized params ----'
+layernames = solver.net.params.keys()
+for l in layernames:
+	print 'yixi', l, np.mean(solver.net.params[l][0].data), np.max(solver.net.params[l][0].data), np.mean(solver.net.params[l][0].data)
+
+
 
 # copy base weights for fine-tuning
 solver.net.copy_from(base_weights)
+
+print '----- yixi initialized params ----'
+layernames = solver.net.params.keys()
+for l in layernames:
+	print 'yixi', l, np.mean(solver.net.params[l][0].data), np.max(solver.net.params[l][0].data), np.mean(solver.net.params[l][0].data)
+
+
 
 # solve straight through -- a better approach is to define a solving loop to
 # 1. take SGD steps
