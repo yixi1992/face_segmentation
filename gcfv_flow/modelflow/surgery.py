0f0faf9d41e50e6c3f47cc382992312336ae7de2
yixi1992 <yixi1992@gmail.com>
2016-04-11 02:09:58 -0400

    finetuning flow from best RGB (finetuned from Camvid) /lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfvshuffle200200/train_lr1e-10/_iter_30000.caffemodel

---
+++ gcfv_flow/modelflow/surgery.py
@@ -0,0 +1,42 @@
+# Make sure that caffe is on the python path:
+caffe_root = './caffe'  # this file is expected to be in {caffe_root}/examples
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+
+
+# Load the fully convolutional network to transplant the parameters.
+net_full_conv = caffe.Net('deploy_modeldefault.prototxt', 
+                          '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfvshuffle200200/train_lr1e-10/_iter_30000.caffemodel', caffe.TEST)
+net = caffe.Net('/lustre/yixi/face_segmentation_finetune/flow/modeldefault/deploy.prototxt', '/lustre/yixi/face_segmentation_finetune/gcfv_flow/modeldefault/snapshots_gcfvshuffle200200/train_lr1e-10/_iter_30000.caffemodel', caffe.TEST)
+
+
+params_full_conv = ['conv1_1_flow']
+conv_params = {pr: [net_full_conv.params[pr][i].data for i in range(len(net_full_conv.params[pr]))] for pr in params_full_conv}
+for conv in params_full_conv:
+	print '{} weights are {} dimensional'.format(conv, conv_params[conv][0].shape) + ('' if len(conv_params[conv])<=1 else 'and biases are {} dimensional'.format(conv_params[conv][1].shape))
+
+
+# Load the original network and extract the fully connected layers' parameters.
+params = ['conv1_1']
+fc_params = {pr: [net.params[pr][i].data for i in range(len(net.params[pr]))] for pr in params}
+for fc in params:
+	print '{} weights are {} dimensional'.format(fc, fc_params[fc][0].shape) + ('' if len(fc_params[fc])<=1 else 'and biases are {} dimensional'.format(fc_params[fc][1].shape))
+
+
+
+
+for pr, pr_conv in zip(params, params_full_conv):
+	for i in range(len(conv_params[pr_conv])):
+		if len(fc_params[pr][i].shape)==4:
+			conv_params[pr_conv][i][0:fc_params[pr][i].shape[0], 0:fc_params[pr][i].shape[1], 0:fc_params[pr][i].shape[2], 0:fc_params[pr][i].shape[3]] = fc_params[pr][i]
+		elif len(fc_params[pr][i].shape)==1:
+			conv_params[pr_conv][i][0:fc_params[pr][i].shape[0]] = fc_params[pr][i]
+		else:
+			print 'no------------------------ the shape is ', fc_params[pr][i].shape
+
+
+
+net_full_conv.save('modeldefaultflow.caffemodel')
+
