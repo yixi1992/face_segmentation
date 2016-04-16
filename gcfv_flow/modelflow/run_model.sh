64d606b1c2f95b4770e4541f6016a99a2e8b052d
yixi1992 <yixi1992@gmail.com>
2016-04-16 03:03:25 -0400

    Finetune from VGG_modeldefault_surg; copy weights then bilinear interp Deconv layers; and fixed lr_mult: 0 for Deconvolution (3layers)

---
+++ gcfv_flow/modelflow/run_model.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -t 4:00:00
+#SBATCH -N 1
+#SBATCH --mail-user=yixi@cs.umd.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --gres=gpu
+
+source /lustre/yixi/decouplednet/DecoupledNet/inference/load_deeplab_dependencies.sh
+source ../../load_deeplab_dependencies.sh
+
+python ./solve.py
