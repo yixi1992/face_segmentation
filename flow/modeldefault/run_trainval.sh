80ce6b1eea0f236426a02ca06f78331d9cc2f0d2
yixi1992 <yixi1992@gmail.com>
2016-04-20 19:02:05 -0400

    apply trainval or train+val trained model

--- flow/modeldefault/run_trainval.sh
+++ flow/modeldefault/run_trainval.sh
@@ -11,4 +11,4 @@
 source /lustre/yixi/decouplednet/DecoupledNet/inference/load_deeplab_dependencies.sh
 source ../../load_deeplab_dependencies.sh
 
-python ./solve.py > solve.out
+./caffe/build/tools/caffe train -solver solver_trainval.prototxt -weights snapshots_tvtcamvidfmp200200/train_lr1e-10/_iter_16000.caffemodel -gpu 0
--- flow/modeldefault/solve.py
+++ flow/modeldefault/solve.py
@@ -31,13 +31,20 @@ def interp_surgery(net, layers):
 # base net -- follow the editing model parameters example to make
 # a fully convolutional VGG16 net.
 # http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/net_surgery.ipynb
-# base_weights = 'vgg16fc.caffemodel'
+#base_weights = 'vgg16fc.caffemodel'
 # base_weights = '../VGG16fc.caffemodel'
 # base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/model2/snapshots_camvid300/train_lr1e-12/_iter_12300.caffemodel'
 # base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/model2/snapshots_camvid300/train_lr1e-12_12300_lr1e-10_12500/_iter_13000.caffemodel'
 # base_weights = '/lustre/yixi/face_segmentation_finetune/fcn8/model1/snapshots_camvid300/train_lr1e-10w0.0005/_iter_12000.caffemodel'
-base_weights = '/lustre/yixi/face_segmentation_finetune/fcn8/model1/snapshots_camvid300/train_lr1e-10w0.0005_12000/_iter_26000.caffemodel'
 #base_weights = '/lustre/yixi/face_segmentation_finetune/flow/model1/snapshots_camvid300flow/train_lr1e-10_4850/_iter_11400.caffemodel'
+
+#base_weights = '/lustre/yixi/face_segmentation_finetune/fcn8/model1/snapshots_camvid300/train_lr1e-10w0.0005_12000/_iter_26000.caffemodel'
+#base_weights = '/lustre/yixi/face_segmentation_finetune/flow/modeldefault/snapshots_camvid200200/train_lr1e-10/_iter_77000.caffemodel'
+
+# for new dataset under segnet trainvaltest split
+#base_weights = '/lustre/yixi/face_segmentation_finetune/fullconv/VGG16fc.caffemodel'
+base_weights = 'snapshots_tvtcamvidfmp200200/train_lr1e-10/_iter_16000.caffemodel'
+
 
 # init
 caffe.set_mode_gpu()
@@ -46,8 +53,8 @@ caffe.set_device(0)
 solver = caffe.SGDSolver('solver.prototxt')
 
 # do net surgery to set the deconvolution weights for bilinear interpolation
-interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
-interp_surgery(solver.net, interp_layers)
+#interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+#interp_surgery(solver.net, interp_layers)
 
 # copy base weights for fine-tuning
 solver.net.copy_from(base_weights)
