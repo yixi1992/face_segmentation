5f49222c7ba4008778ea16e8f8b5cda8a191b761
yixi1992 <yixi1992@gmail.com>
2016-04-21 13:19:39 -0400

    Applying trainval from vgg16 for lr1e-10

--- flow/modeldefault/run_trainval.sh
+++ flow/modeldefault/run_trainval.sh
@@ -11,4 +11,5 @@
 source /lustre/yixi/decouplednet/DecoupledNet/inference/load_deeplab_dependencies.sh
 source ../../load_deeplab_dependencies.sh
 
-./caffe/build/tools/caffe train -solver solver_trainval.prototxt -weights snapshots_tvtcamvidfmp200200/train_lr1e-10/_iter_16000.caffemodel -gpu 0
+python solve_trainval.py
+#./caffe/build/tools/caffe train -solver solver_trainval.prototxt -weights /lustre/yixi/face_segmentation_finetune/fullconv/VGG16fc.caffemodel -gpu 0 -iterations 16000
